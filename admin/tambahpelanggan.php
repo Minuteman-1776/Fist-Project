a62c8292e77d759ab29e977180a56bdcb6a28447
Minuteman-1776 <alfatah1776@gmail.com>
2020-08-21 10:55:43 +0700

    change

--- admin/tambahpelanggan.php
+++ admin/tambahpelanggan.php
@@ -10,6 +10,14 @@
         <input type="text" class="form-control" name="email">
     </div>
     <div class="form-group">
+        <label>Password</label>
+        <input type="text" class="form-control" name="password">
+    </div>
+    <div class="form-group">
+        <label>Alamat</label>
+        <input type="text" class="form-control" name="alamat">
+    </div>
+    <div class="form-group">
         <label>No. Telepon</label>
         <input type="number" class="form-control" name="notelepon">
     </div>
@@ -19,8 +27,8 @@
 <?php
     if (isset($_POST['save']))
     {
-        $koneksi->query("INSERT INTO pelanggan(nama_pelanggan,email_pelanggan,telepon_pelanggan)
-        VALUES('$_POST[nama]','$_POST[email]','$_POST[notelepon]')");
+        $koneksi->query("INSERT INTO pelanggan(nama_pelanggan,email_pelanggan,telepon_pelanggan,password_pelanggan,alamat_pelanggan)
+        VALUES('$_POST[nama]','$_POST[email]','$_POST[notelepon]','$_POST[password]','$_POST[alamat]')");
 
         echo "<div class='alert alert-info'>Data tersimpan</div>";
         echo "<meta http-equiv='refresh' content='1;url=index.php?halaman=pelanggan'>";
